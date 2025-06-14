744092f7ddf624d5ea226798200dc1dde0193ecf
Manh-Hao Pham <manhhaoo.do@gmail.com>
2025-06-14 02:04:25 +0200

    Add Reading Field to differentiate Expressions, Fix error handling, Layout Adjustments, README

--- README.md
+++ README.md
@@ -7,19 +7,21 @@ This is a basic Anki add-on to backfill fields using [Yomitan's API](https://git
 3. Restart Anki
 
 ## Usage
-1. **Create a Backup of your profile/deck**
-2. Go to `Tools -> Backfill from Yomitan` in the top bar
-3. Select your deck in the `Deck` dropdown
-4. For `Expression Field` choose the expression field (e.g. `Expression` in Lapis, `word` in Senren) of your note type, this is the field that will be queried into Yomitan
-5. For `Field` choose the field to be backfilled
-6. In `Handlebar` type in the Yomitan handlebar, from which you wish to pull data from, without brackets (e.g. `frequency-harmonic-rank`) 
-7. Optionally tick `Replace` if you wish to replace the current content of the field in every card
-8. Press `Run`
+1.  **Create a Backup of your profile/deck**
+2. Make sure your Browser is running and the API is working.
+3. Go to `Tools -> Backfill from Yomitan` in the top bar.
+4. Select your deck in the `Deck` dropdown.
+5. For `Expression Field` choose the expression field (e.g. `Expression` in Lapis) of your note type, this is the field that will be queried into Yomitan.
+6. Optionally choose a `Reading Field` (e.g. ExpressionReading in Lapis) to differentiate expressions using their reading. If left blank, the add-on uses the first result Yomitan returns.
+7. For `Field` choose the field to be backfilled.
+8. In `Handlebar` type in the Yomitan handlebar, from which you wish to pull data from, without brackets (e.g. `frequency-harmonic-rank`).
+9. Optionally tick `Replace` if you wish to replace the current content of the field in every card.
+10. Press `Run`.
 
-Changes can be undone with `Edit -> Undo` or with `CTRL + Z`
+Changes can be undone with `Edit -> Undo` or with `CTRL + Z`.
 
 ## Issues
-The add-on currently only pulls the first result from the API and can therefore not handle different readings (it will fill identical expressions with the same data).
+The add-on can currently only run on the entire deck.
 
 ## Screenshot
 ![screenshot](https://github.com/Manhhao/backfill-anki-yomitan/blob/main/screenshot/image.png?raw=true)
--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@ request_timeout = 10
 ping_timeout = 5
 
 # https://github.com/Kuuuube/yomitan-api/blob/master/docs/api_paths/ankiFields.md
-def request_handlebar(expression, handlebar):
+def request_handlebar(expression, reading, handlebar):
     body = {
         "text": expression,
         "type": "term",
         "markers": [handlebar, "reading"],
-        "maxEntries": 1,
+        "maxEntries": 4 if reading else 1, # should probably be configurable
     }
 
     req = urllib.request.Request(
@@ -32,7 +32,8 @@ def request_handlebar(expression, handlebar):
         data = json.loads(response.read())
     except HTTPError as e:
         if e.code == 500:
-            raise ValueError("Invalid handlebar specified")
+            # this throws if the handlebar does not exist for specified term
+            return None
         else:
             raise
     except URLError as e:
@@ -42,12 +43,18 @@ def request_handlebar(expression, handlebar):
     if not data:
         return None
     
-    # todo: optionally query reading, cycle through x amount of results and 
-    # return handlebar if expression and reading match, else None
-    return data[0].get(handlebar)
+    if reading:
+        for entry in data:
+            if entry.get("reading") == reading:
+                return entry.get(handlebar)
+        
+        # no entry matching reading, skip by returning None
+        return None
+    else:
+        return data[0].get(handlebar)
 
 def ping_yomitan(): 
-    req = urllib.request.Request(request_url + "/serverVersion", method="POST")
+    req = urllib.request.Request(request_url + "/yomitanVersion", method="POST")
     try:
         response = urllib.request.urlopen(req, timeout=ping_timeout)  
         data = json.loads(response.read())
@@ -61,7 +68,8 @@ def open_dialog():
         return
     
     dlg = BackfillDialog(mw)
-    
+    dlg.resize(350, dlg.height())
+
     if hasattr(dlg, "exec_"):
         # qt5
         dlg.exec_()
@@ -77,24 +85,31 @@ class BackfillDialog(QDialog):
         self.decks = QComboBox()
         self.fields = QComboBox()
         self.expression_field = QComboBox()
+        self.reading_field = QComboBox()
         self.yomitan_handlebar = QLineEdit()
         self.apply = QPushButton("Run")
         self.cancel = QPushButton("Cancel")
         self.replace = QCheckBox("Replace");
-        
+
         form = QFormLayout()
+        form.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.AllNonFixedFieldsGrow)
+        form.setLabelAlignment(Qt.AlignmentFlag.AlignLeft)
         form.addRow(QLabel("Deck:"), self.decks)
         form.addRow(QLabel("Expression Field:"), self.expression_field)
+        form.addRow(QLabel("Reading Field:"), self.reading_field)
         form.addRow(QLabel("Field:"), self.fields)
         form.addRow(QLabel("Handlebar:"), self.yomitan_handlebar)
-        form.addRow(self.replace)
-        
+
         buttons = QHBoxLayout()
         buttons.addWidget(self.apply)
         buttons.addWidget(self.cancel)
 
+        checkboxes = QHBoxLayout()
+        checkboxes.addWidget(self.replace)
+
         layout = QVBoxLayout()
         layout.addLayout(form)
+        layout.addLayout(checkboxes)
         layout.addLayout(buttons)
         self.setLayout(layout)
 
@@ -116,7 +131,8 @@ class BackfillDialog(QDialog):
     def _update_fields(self):
         self.fields.clear()
         self.expression_field.clear()
-        
+        self.reading_field.clear()
+
         deck_id = self.decks.currentData()
         if deck_id is None:
             return
@@ -132,10 +148,14 @@ class BackfillDialog(QDialog):
         for name in sorted(field_names):
             self.fields.addItem(name)
             self.expression_field.addItem(name)
+            self.reading_field.addItem(name)
+
+        self.reading_field.setCurrentIndex(-1)
     
     def _on_run(self):
         deck_id = self.decks.currentData()
         expression_field = self.expression_field.currentText()
+        reading_field = self.reading_field.currentText()
         field = self.fields.currentText()
         handlebar = self.yomitan_handlebar.text()
         should_replace = self.replace.isChecked()
@@ -150,7 +170,8 @@ class BackfillDialog(QDialog):
                 if expression_field in note and field in note:
                     current = note[field].strip()
                     if should_replace or not current:
-                        data = request_handlebar(note[expression_field].strip(), handlebar)
+                        reading = note[reading_field] if reading_field else None
+                        data = request_handlebar(note[expression_field].strip(), reading, handlebar)
                         if data:
                             note[field] = data
                             notes.append(note)
