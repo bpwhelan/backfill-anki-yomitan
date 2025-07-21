fcdae1f2cba318e1571ac93c4cd8983714116aec
Manhhao <manhhaoo.do@gmail.com>
2025-07-21 21:38:54 +0200

    split yomitan api into own file

--- __init__.py
+++ __init__.py
@@ -7,53 +7,10 @@ from aqt.utils import showInfo, showWarning
 from aqt.qt import *
 from urllib.error import HTTPError, URLError
 from .browser import BrowserBackfill
-
-request_url = "http://127.0.0.1:8766"
-request_timeout = 10
-ping_timeout = 5
-
-# https://github.com/Kuuuube/yomitan-api/blob/master/docs/api_paths/ankiFields.md
-def request_handlebar(expression, reading, handlebar):
-    body = {
-        "text": expression,
-        "type": "term",
-        "markers": [handlebar, "reading"],
-        "maxEntries": 4 if reading else 1, # should probably be configurable
-        "includeMedia": True
-    }
-
-    req = urllib.request.Request(
-        request_url + "/ankiFields",
-        data=json.dumps(body).encode("utf-8"),
-        headers={"Content-Type": "application/json"},
-        method="POST",
-    )
-
-    try:
-        response = urllib.request.urlopen(req, timeout=request_timeout)  
-        data = json.loads(response.read())
-    except HTTPError as e:
-        if e.code == 500:
-            # this throws if the handlebar does not exist for specified term
-            return None
-        else:
-            raise
-    except URLError as e:
-        raise ConnectionRefusedError(f"Request to Yomitan API failed: {e.reason}")
-    
-    return data
-
-def ping_yomitan(): 
-    req = urllib.request.Request(request_url + "/yomitanVersion", method="POST")
-    try:
-        response = urllib.request.urlopen(req, timeout=ping_timeout)  
-        data = json.loads(response.read())
-        return data
-    except Exception:
-        return False
+from . import yomitan_api  
 
 def open_dialog():
-    if not ping_yomitan():
+    if not yomitan_api.ping_yomitan():
         showWarning("Unable to reach Yomitan API")
         return
     
@@ -188,7 +145,7 @@ class BackfillDialog(QDialog):
                 current = note[field].strip()
                 if should_replace or not current:
                     reading = note[reading_field] if reading_field else None
-                    api_request = request_handlebar(note[expression_field].strip(), reading, handlebar)
+                    api_request = yomitan_api.request_handlebar(note[expression_field].strip(), reading, handlebar)
                     if not api_request:
                         continue
 
